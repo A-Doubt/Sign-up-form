d38c0f048b1f578fa95acd15c6da22aa3deecf2f
A-Doubt <adoubt2@gmail.com>
2022-02-09 02:56:02 +0100

    Project complete.
    Since last commit:
    1. added passoword validation
    2. added password strength indicators
    3. removed placeholder logo and added a new one
    4. fixed layout
    5. Tidied up the code
    6. fixed bugs

--- script.js
+++ script.js
@@ -1,64 +1,16 @@
+// get password inputs DOM and add listeners
 const pwdInput = document.querySelector('#pwd');
 const pwdInputConfirm = document.querySelector('#pwd-confirm');
+// listeners to validate is password inputs match each other
+// and if password matches required pattern
+pwdInput.addEventListener('input', validatePasswordMatch);
+pwdInputConfirm.addEventListener('input', validatePasswordMatch);
+pwdInput.addEventListener('input', validatePassword);
 
-pwdInput.addEventListener('input', checkPassowrdMatch);
-pwdInputConfirm.addEventListener('input', checkPassowrdMatch);
-
+// get DOM to update message that passwords have to match 
 const pwdMatch = document.querySelector('.pwd-match');
-const pwdLength = document.querySelector('.pwd-length');
-
-pwdMatch.textContent = 'Passwords have to match!';
-pwdMatch.classList.add('invisible');
-
+// the whole password fieldset to enlarge it when the message shows
 const pwdFieldset = document.querySelector('.pwd-fieldset');
-
-// check if both password inputs match
-function checkPassowrdMatch() {
-    if (!pwdInput.value && !pwdInputConfirm.value) return;
-    if (pwdInput.value != pwdInputConfirm.value) {
-        // pwdInput.classList.add('invalid');
-        // pwdInputConfirm.classList.add('invalid');
-        pwdMatch.classList.remove('invisible');
-        pwdFieldset.classList.add('enlarged');
-    }
-    else {
-        // pwdInput.classList.remove('invalid');
-        // pwdInputConfirm.classList.remove('invalid');
-        pwdMatch.classList.add('invisible')
-        pwdFieldset.classList.remove('enlarged');
-    }
-}
-
-pwdInput.addEventListener('input', validatePassowrd);
-pwdInputConfirm.addEventListener('input', validatePassowrd);
-
-// check if password has at least 1 lowercase letter, 1 uppercase letter, 1 special character and is long enough.
-function validatePassowrd() {
-    if(pwdInput.value.length < 8 || pwdInput.value.length > 20) {
-
-    }
-    if (pwdInput.validity.valid) {
-        pwdInput.classList.add('pwd-valid');
-    }
-    else {
-        pwdInput.classList.remove('pwd-valid');
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 pwdFieldset.appendChild(pwdMatch);
 const inputs = document.querySelectorAll('input');
@@ -77,5 +29,92 @@ inputs.forEach((input) => {
     };
 });
     
+let strength = 0;
+function validatePassword(e) {
+    let validations = [];
 
+    const password = e.target.value;
+    const pwdLi = document.querySelectorAll('.pwd-li');
 
+    validations = [
+        (password.length >= 8),
+        (password.search(/[A-Z]/) > -1),
+        (password.search(/[0-9]/) > -1),
+        (password.search(/[!@#$%^&*()~`{}:"|<>?/.,\';]/) > -1)
+    ]
+    strength = validations.reduce((accumulated, current) => accumulated + current);
+
+    // update markers
+    function updateMarkers() {
+        for (let i = 0; i < 4; i++) {
+            if (validations[i]) {
+                pwdLi[i].classList.remove('cross');
+                pwdLi[i].classList.add('tick');
+            }
+            else {
+                pwdLi[i].classList.add('cross');
+                pwdLi[i].classList.remove('tick');
+            }
+        }
+    };
+    updateMarkers();
+    
+    // update strength bars
+    function updateBars () {
+        const bars = document.querySelectorAll('.bar');
+        switch (strength) {
+            case 0:
+                bars[0].classList.remove('bar-show');
+                bars[1].classList.remove('bar-show');
+                bars[2].classList.remove('bar-show');
+                bars[3].classList.remove('bar-show');
+                break;
+            case 1:
+                bars[0].classList.add('bar-show');
+                bars[1].classList.remove('bar-show');
+                bars[2].classList.remove('bar-show');
+                bars[3].classList.remove('bar-show');
+                break;
+            case 2:
+                bars[0].classList.add('bar-show');
+                bars[1].classList.add('bar-show');
+                bars[2].classList.remove('bar-show');
+                bars[3].classList.remove('bar-show');
+                break;
+            case 3:
+                bars[0].classList.add('bar-show');
+                bars[1].classList.add('bar-show');
+                bars[2].classList.add('bar-show');
+                bars[3].classList.remove('bar-show');
+                break;
+            case 4:
+                bars[0].classList.add('bar-show');
+                bars[1].classList.add('bar-show');
+                bars[2].classList.add('bar-show');
+                bars[3].classList.add('bar-show');
+                break;
+        }
+    }
+    updateBars();
+}
+
+function validatePasswordMatch(e) {
+
+        // if (!pwdInput.value && !pwdInputConfirm.value) return; //on 0 character password
+        if (pwdInput.value != pwdInputConfirm.value) {
+            pwdMatch.classList.remove('invisible');
+            pwdFieldset.classList.add('enlarged');
+        }
+        else {
+            pwdMatch.classList.add('invisible')
+            pwdFieldset.classList.remove('enlarged');
+        }
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', submitForm);
+function submitForm(e) {
+    if (pwdInput.value != pwdInputConfirm.value) {
+        e.preventDefault();
+    }
+}
